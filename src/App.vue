c1477ca1b3c4529ead885a052271ff3031d916ae
ITstart <itstart@itstep.loc>
2022-04-11 17:51:21 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <RouterLink to ="/"> Home </RouterLink>
+ <RouterLink to ="/about"> About </RouterLink>
+  <RouterView />
+</div>
+ 
+</template>
+
+<script>
+
+
+</script>
+<style>
+
+</style>
